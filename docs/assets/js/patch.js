8fb4b9ea579f753eeebcb6f4801c14e78a9f5dc9
Cael <MR-Addict@qq.com>
2025-07-04 00:25:37 +0800

    chore: bump version to 0.2.17-rc.2 and update related documentation

--- docs/assets/js/patch.js
+++ docs/assets/js/patch.js
@@ -1,6 +1,6 @@
 function appendVersion() {
   const shield = document.createElement("span");
-  shield.textContent = "02.17-rc.1";
+  shield.textContent = "0.2.17-rc.2";
   document.querySelector(".menu-title").appendChild(shield);
 }
 
