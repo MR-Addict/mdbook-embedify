420fc8fa3cb991c0a50a7006315eb984e123e4c8
Cael <MR-Addict@qq.com>
2025-07-04 23:53:09 +0800

    fix: improve mdbook installation step to check for existing installation

--- docs/assets/js/patch.js
+++ docs/assets/js/patch.js
@@ -1,6 +1,6 @@
 function appendVersion() {
   const shield = document.createElement("span");
-  shield.textContent = "0.2.17-rc.11";
+  shield.textContent = "0.2.17-rc.12";
   document.querySelector(".menu-title").appendChild(shield);
 }
 
